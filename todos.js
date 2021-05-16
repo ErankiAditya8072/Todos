4a3fc213e99ac849ff0c87002211afd43731ba90
Eranki Aditya <erankiaditya27@gmail.com>
2021-05-16 11:41:54 +0530

    removed changes

--- todos.js
+++ todos.js
@@ -3,13 +3,6 @@ window.onload= function()
     var tab=document.getElementById("tab");
     var but= document.getElementById("btn");
     var tx=document.getElementById("addtext");
-    var m=window.matchMedia("(orientation:landscape)");
-    m.addEventListener("orientationchange",function(){
-        if(m.matches)
-        {
-            changedeleticon();
-        }
-    },false);
     tx.addEventListener('keypress',function(e){
           if (e.keyCode === 13 )
           {
@@ -52,20 +45,10 @@ function addrow()
         r2.className="cols1";
         var r3=document.createTextNode(txt.value);
         r1.id="tr"+count;
-
         var r4 = document.createElement('td');
         var r5= document.createElement('button');
         r5.className="desbtn";
-        console.log(window.screen.width);
-        if (window.screen.width < 600)
-        {
-            r5.innerHTML='<i class="fas fa-trash-alt fa-sm"></i>';
-            console.log("300px");
-        }
-        else
-        {
-           r5.innerHTML='<i class="fas fa-trash-alt fa-lg"></i>';
-        }
+        r5.innerHTML='<i class="fas fa-trash-alt fa-sm"></i>';
         r4.appendChild(r5);
         r2.appendChild(r3);
         r1.appendChild(r2);
@@ -87,14 +70,4 @@ function deleterow(e)
     var t1=e.target.parentNode.parentNode;
     tab.removeChild(t1);
 }
-function changedeleticon(){
-    var d= document.getElementsByClassName("desbtn");
-    if (d.length > 0)
-    {
-        for(var i=0;i<d.length;i++)
-        {
-            d[0].innerHTML='<i class="fas fa-trash-alt fa-lg"></i>';
-        }
 
-    }
-}
